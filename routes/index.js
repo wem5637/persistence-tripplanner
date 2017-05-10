af69f01f9c172f3551ce80116e5f00cadce391a8
Will McCracken <wem5637@gmail.com>
2017-05-10 15:22:51 -0500

    added api routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var Hotel = require('../models/hotel');
 var Restaurant = require('../models/restaurant');
 var Activity = require('../models/activity');
 
+
+
 router.get('/', function(req, res, next) {
   Promise.all([
     Hotel.findAll(),
@@ -19,5 +21,34 @@ router.get('/', function(req, res, next) {
   })
   .catch(next);
 });
+
+
+router.route('/api/hotels').get(function(req, res, next){
+  var arr;
+  Hotel.findAll()
+  .then(function(hotels){
+    arr = hotels;
+    res.json(arr);
+  })
+})
+
+router.route('/api/restaurants').get(function(req, res, next){
+  var arr;
+  Hotel.findAll()
+  .then(function(restaurants){
+    arr = restaurants;
+    res.json(arr);
+  })
+})
+
+router.route('/api/activities').get(function(req, res, next){
+  var arr;
+  Hotel.findAll()
+  .then(function(activities){
+    arr = activities;
+    res.json(arr);
+  })
+})
+
 
 module.exports = router;
